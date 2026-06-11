4e7cd1ce42add8de3a4fede57f42c997288c6948
Tr100000 <147051084+Tr100000@users.noreply.github.com>
2026-06-11 20:27:04 +0900

    re-enable fabric model loading api support

--- src/client/java/io/github/tr100000/codec2schema/compat/fabric/FabricCompatClient.java
+++ src/client/java/io/github/tr100000/codec2schema/compat/fabric/FabricCompatClient.java
@@ -2,12 +2,13 @@ package io.github.tr100000.codec2schema.compat.fabric;
 
 import io.github.tr100000.codec2schema.ModUtils;
 import io.github.tr100000.codec2schema.api.Codec2SchemaPlugin;
+import io.github.tr100000.codec2schema.api.MapCodecHandlerRegistry;
 
 public class FabricCompatClient implements Codec2SchemaPlugin {
     @Override
     public void registerHandlers() {
         if (ModUtils.hasFabricModelLoadingApi()) {
-//            MapCodecHandlerRegistry.register(KeyExistsCodecHandler::predicate, KeyExistsCodecHandler::new);
+            MapCodecHandlerRegistry.register(KeyExistsCodecHandler::predicate, KeyExistsCodecHandler::new);
         }
     }
 }
